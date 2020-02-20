f07bcc322ff1471d3953a20cd97424f308fde526
Srinivasa Arudra Phani Bhushan Kolla <bhushankolla@usf.edu>
2020-02-20 16:54:21 -0500

    Add project files.

---
+++ PracticeApplication/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace PracticeApplication
+{
+    class Program
+    {
+        
+        static void Main2(string[] args)
+        {
+
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee() { Exp = 5, ID = 1, Name = "Phani", Salary = 1500000 });
+            empList.Add(new Employee() { Exp = 3, ID = 3, Name = "Rudraa", Salary = 2000000 });
+            empList.Add(new Employee() { Exp = 2, ID = 5, Name = "Srinivas", Salary = 1000000 });
+            empList.Add(new Employee() { Exp = 4, ID = 7, Name = "Bhushan", Salary = 9000000 });
+
+            //using delgates with lamda expressions. 
+            //here the second parameter of the promotionCheck method is PromotionDelegate type, which inturn takes type Employee as 
+            //input parameter.
+            Employee.promotionCheck(empList, emp => emp.Exp>2);
+
+            //using delegates with methods.
+            Employee.promotionCheck(empList, promoCheckMethod1);
+
+            //initializing an instance of delegate
+            PromotionDelegate promDel = new PromotionDelegate(promoCheckMethod1);
+            //passing the delegate as a paramaeter the method.
+            Employee.promotionCheck(empList, promDel);
+        }
+
+        public static bool promoCheckMethod1(Employee emp)
+        {
+            if (emp.Exp > 2)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool promoCheckMethod2(Employee emp)
+        {
+            if (emp.Salary > 1500000)
+                return true;
+            else
+                return false;
+        }
+
+    }
+
+    //Step 1 Create a delegate, based on the return type and input we want to replace and decouple.
+    delegate bool PromotionDelegate(Employee empl);
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Exp { get; set; }
+        
+        //passing the method or delegate or lamda expression as a parameter. Doing so, we can dyamically pass our own function and
+        //declare our own criteria for promotion.
+        public static void promotionCheck(List<Employee> empList, PromotionDelegate isPromotable)
+        {
+            foreach(Employee emp in empList)
+            {
+                if(isPromotable(emp))
+                {
+                    Console.WriteLine("{0} is promoted",emp.Name);
+                }
+            }
+        }
+    }
+}
---
+++ PracticeApplication/reverseStringPractice.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace PracticeApplication
+{
+    class reverseStringPractice
+    {
+
+        public static void Main()
+        {
+            char[] inp = new char[] {'a' , 'b' , 'c' , 'a' , 'b' , 'c' };
+            int k = 2;
+            bool flag = false;
+            Dictionary<char, List<int>> dict = new Dictionary<char, List<int>>();
+            List<int> tempList = new List<int>();
+           
+            
+
+            for (int i = 0; i < inp.Length; i++)
+            {
+                List<int> uniqueList = new List<int>();
+                if (!dict.ContainsKey(inp[i]))
+                {
+                    
+                    uniqueList.Add(i);
+                    dict.Add(inp[i], uniqueList);
+                }
+                else
+                {
+                    tempList = dict[inp[i]];
+                    tempList.Add(i);
+                    dict[inp[i]] = tempList;
+
+                }
+                
+            }
+            foreach(var temp in dict)
+            {
+                if (temp.Value.Count > 1)
+                {
+                    for (int i = 0; i < temp.Value.Count - 1; i++)
+                    {
+                        if ((temp.Value[i + 1] - temp.Value[i]) <= k)
+                        {
+                            flag = true;
+                            break;
+                        }
+                       
+                    }
+                    if (flag == true)
+                        break;
+                }
+            }
+            Console.WriteLine(flag);
+        }
+
+    }
+}
